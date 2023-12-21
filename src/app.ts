6ba76209f47708c71139fae7af432b1ceea3f6fe
Leonardo Henrique <leonardo0507.business@gmail.com>
2023-12-21 17:55:27 -0300

    feat: create food routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastifyCookie from "@fastify/cookie";
 import fastify from "fastify";
 import { authRoutes } from "./routes/auth";
+import { foodRoutes } from "./routes/foods";
 
 export const app = fastify();
 
 app.register(fastifyCookie);
 app.register(authRoutes, { prefix: "auth" });
+app.register(foodRoutes, { prefix: "foods" });
---
+++ src/routes/foods.ts
@@ -0,0 +1,107 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { database } from "../database";
+import { session } from "../middlewares/session";
+import { validate } from "../middlewares/validate";
+
+export async function foodRoutes(app: FastifyInstance) {
+  const postFoodBodySchema = z.object({
+    name: z.string(),
+    inDiet: z.boolean(),
+    description: z.string().optional(),
+    createdAt: z.coerce.date().optional(),
+  });
+
+  app.post<{ Body: z.infer<typeof postFoodBodySchema> }>(
+    "/",
+    { preHandler: [session, validate(postFoodBodySchema)] },
+    async (req, res) => {
+      const { authUserId, body } = req;
+
+      await database("foods").insert({ userId: authUserId, ...body });
+
+      res.code(201).send();
+    },
+  );
+
+  app.get("/", { preHandler: [session] }, async (req, res) => {
+    const { authUserId } = req;
+
+    const foods = await database("foods").where({ userId: authUserId });
+
+    res.send({ foods });
+  });
+
+  const getFoodParamsSchema = z.object({
+    id: z.string().uuid(),
+  });
+
+  app.get<{ Params: z.infer<typeof getFoodParamsSchema> }>(
+    "/:id",
+    { preHandler: [session, validate(getFoodParamsSchema, "params")] },
+    async (req, res) => {
+      const { authUserId } = req;
+      const { id } = req.params;
+
+      const food = await database("foods")
+        .where({ userId: authUserId, id })
+        .first();
+
+      if (!food) return res.code(404).send({ error: "Food not found." });
+
+      res.send({ food });
+    },
+  );
+
+  const putFoodBodySchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    inDiet: z.boolean().optional(),
+    createdAt: z.coerce.date().optional(),
+  });
+
+  app.put<{
+    Params: z.infer<typeof getFoodParamsSchema>;
+    Body: z.infer<typeof putFoodBodySchema>;
+  }>(
+    "/:id",
+    {
+      preHandler: [
+        session,
+        validate(getFoodParamsSchema, "params"),
+        validate(putFoodBodySchema),
+      ],
+    },
+    async (req, res) => {
+      const { authUserId } = req;
+      const { id } = req.params;
+
+      const food = database("foods").where({ userId: authUserId, id });
+
+      if (!(await food.first()))
+        return res.code(404).send({ error: "Food not found." });
+
+      await food.update(req.body);
+
+      res.code(204).send();
+    },
+  );
+
+  app.delete<{ Params: z.infer<typeof getFoodParamsSchema> }>(
+    "/:id",
+    { preHandler: [session, validate(getFoodParamsSchema, "params")] },
+    async (req, res) => {
+      const { authUserId } = req;
+      const { id } = req.params;
+
+      const food = database("foods").where({ userId: authUserId, id });
+
+      if (!(await food.first()))
+        return res.code(404).send({ error: "Food not found." });
+
+      await food.del();
+
+      res.code(204).send();
+    },
+  );
+}
